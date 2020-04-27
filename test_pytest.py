f9b07131b7408991a91e45af484e71f09ffe9346
mrstreth <mr.tesanta@gmail.com>
2020-04-27 16:05:24 +0500

    test some fix

--- test_pytest.py
+++ test_pytest.py
@@ -1,29 +1,62 @@
 import pytest
-import main
+#import main
 import os.path
+#-------------------------------------------------
+name_database = 'database.db'
+db = SqliteDatabase(name_database)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+             
+class CLIENTS(BaseModel):
+    """Table CLIENTS"""
+    
+    ID = PrimaryKeyField()
+    NAME = CharField(max_length=50)
+    CITY = CharField(max_length=50)
+    ADDRESS = CharField(max_length=50)
+    
+    class Meta:
+        db_table = 'clients'
+        order_by = ('ID',)
+                
+class ODDERS(BaseModel):
+    """Table ODDERS"""
+    
+    ID = PrimaryKeyField()
+    CLIENT = ForeignKeyField(CLIENTS,backref='client')
+    DATE = DateTimeField(default=datetime.datetime.today())
+    AMOUNT = IntegerField()
+    DESCRIPTION = CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'odders'
+        order_by = ('ID',)
+#-------------------------------------------------
 
 def test_exists_database():
     """Is there a database"""
-    assert os.path.exists(main.name_database) == True
+    assert os.path.exists(name_database) == True
 
 def test_row_count_clients():
     """Number of Filled Rows in clients = 10"""
-    assert len(main.CLIENTS.select()) == 10
+    assert len(CLIENTS.select()) == 10
 
 def test_row_count_odders():
     """Number of Filled Rows in odders = 10"""
-    assert len(main.ODDERS.select()) == 10
+    assert len(ODDERS.select()) == 10
 
 def test_coll_clients():
     """Does clients contain necessary columns"""
     try:
-        x = main.CLIENTS.select(main.CLIENTS.ID,main.CLIENTS.NAME,main.CLIENTS.CITY, main.CLIENTS.ADDRESS)
+        x = CLIENTS.select(CLIENTS.ID,CLIENTS.NAME,CLIENTS.CITY,CLIENTS.ADDRESS)
     except:
         assert False
 
 def test_coll_odders():
     """Does odders contain necessary columns"""
     try:
-        x = main.ODDERS.select(main.ODDERS.ID,main.ODDERS.CLIENT_id,main.ODDERS.DATE, main.ODDERS.AMOUNT,main.ODDERS.DESCRIPTION)
+        x = ODDERS.select(ODDERS.ID,ODDERS.CLIENT_id,ODDERS.DATE, ODDERS.AMOUNT,ODDERS.DESCRIPTION)
     except:
         assert False
